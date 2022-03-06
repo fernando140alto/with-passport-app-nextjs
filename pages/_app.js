cda3da7ca07d0b239e1052c59adda5c01b658c3e
Fernando <esponges@gmail.com>
2022-03-06 14:02:06 -0600

    init store with redux example

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,31 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { StoreProvider } from "../store/context/context";
-import store from "../store/redux/store";
+import { useStore } from '../store/redux/store';
 
-function MyApp({ Component, pageProps, props }) {
+function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout ?? ((page) => page);
+  const store = useStore(pageProps.initialReduxState);
 
   return getLayout(
     <Provider store={store}>
-      <StoreProvider initialData={props}>
+      <StoreProvider initialData={pageProps}>
         <Component {...pageProps} />
       </StoreProvider>
     </Provider>
   );
 }
 
+export default MyApp;
+
 MyApp.getInitialProps = async () => {
-  // store.dispatch(increment());
-  // fetch some initial data to provide to the store
-  const initialState = await Promise.resolve({
+  /* this data is STATIC data and will always
+  be reset during navigation */
+  const initialReduxState = await Promise.resolve({
     counter: {
       value: 1,
     },
   });
 
-  return initialState;
+  return { pageProps: { initialReduxState } };
 }
-
-export default MyApp;
