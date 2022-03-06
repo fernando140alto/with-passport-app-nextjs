ec12a00870d727ea49cc1adbdd22213402a6689e
Fernando <esponges@gmail.com>
2022-03-05 19:10:53 -0600

    wrap redux store

--- pages/_app.js
+++ pages/_app.js
@@ -1,36 +1,35 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { StoreProvider } from "../store/context/context";
-import configureStore from "../store/redux/store";
+import { wrapper } from "../store/redux/store";
 // import { increment } from "../store/redux/reducer";
 import store from "../store/redux/store";
 function MyApp({ Component, pageProps, props }) {
   const getLayout = Component.getLayout ?? ((page) => page);
-  console.log(props);
+  // console.log(props);
 
   return getLayout(
-    <Provider store={configureStore(props)}>
       <StoreProvider initialData={props}>
         <Component {...pageProps} />
       </StoreProvider>
-    </Provider>
   );
 }
 
-export default MyApp;
 
-MyApp.getInitialProps = async () => {
-  console.log("inital props being called");
+export default wrapper.withRedux(MyApp);
+
+// MyApp.getInitialProps = async () => {
+//   console.log("inital props being called");
   
 
-  // fetch some initial data to provide to the store
-  const initialState = await Promise.resolve({
-    counter: {
-      value: 1,
-    },
-  });
+//   // fetch some initial data to provide to the store
+//   const initialState = await Promise.resolve({
+//     counter: {
+//       value: 1,
+//     },
+//   });
 
-  return {
-    props: initialState,
-  };
-};
+//   return {
+//     props: initialState,
+//   };
+// };
--- store/redux/store.js
+++ store/redux/store.js
@@ -1,18 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit'
 import counterReducer from './reducer'
+import { createWrapper } from 'next-redux-wrapper'
 
-// const store = configureStore({
-//   reducer: {
-//     counter: counterReducer.reducer,
-//   },
-// })
+const store = () => configureStore({
+  reducer: {
+    counter: counterReducer.reducer,
+  },
+})
 
-// export default store;
+export const wrapper = createWrapper(store);
 
-
-export default function (preloadedState) {
-  return configureStore({
-    reducer: { counter: counterReducer.reducer },
-    preloadedState,
-  })
-}
+// export default function (preloadedState) {
+//   return configureStore({
+//     reducer: { counter: counterReducer.reducer },
+//     preloadedState,
+//   })
+// }
